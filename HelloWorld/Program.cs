ebdb4fb9f1bbef632b57458d340f3c89dad0e2e6
Santiago <sg2601710@gmail.com>
2024-02-25 14:51:20 -0300

    first commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWorld
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //caca
+            /* 
+             Manga de pelotudos los odio a todos
+             */
+                Console.WriteLine(3+3);
+            Console.WriteLine("Hello chupapijas");
+        }
+    }
+}
