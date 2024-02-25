08a325a71a229e8035980a1fceaacad9ee6dce81
Santiago <sg2601710@gmail.com>
2024-02-25 15:27:20 -0300

    the math function

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,11 +8,9 @@ namespace HelloWorld
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter username: ");
-
-            string username = Console.ReadLine();
-
-            Console.WriteLine("The username is: " + username);
+            Console.WriteLine(Math.Round(5.5));
+            Console.WriteLine(Math.Max(5, 100));
+            Console.WriteLine(Math.Min(5, 100));
         }
     }
 }
